1d04bdd287fd779c1d49ad7002df5541fcbf3828
killshotroxs <krishnalpanara0066@gmail.com>
2024-01-03 13:34:33 +0530

    bug analytics removed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header/Header";
 import DomainGenerator from "./Components/Domaingeneration/Domainai";
 import Pricing from "./Components/Pricing/Pricing";
-import { Analytics } from '@vercel/analytics/react'; //Not working for now... look at it later(Don't forget to remove below render part.)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Analytics />
     <App />
     <Header />
     <DomainGenerator />
